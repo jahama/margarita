00efb3af8235a1cb35a492cc7c8430bcb9b4da14
Biel <bielbcm@gmail.com>
2018-10-03 13:18:43 +0200

    add module.exports

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import MCol from './MCol/MCol.vue'
 import MContainer from './MContainer/MContainer.vue'
 import MRow from './MRow/MRow.vue'
 
-export default {
+module.exports = {
   MCol,
   MContainer,
   MRow
