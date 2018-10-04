f657500eff65d149bb6d60e28e11006effb12917
Anna Collins <annacollins85@gmail.com>
2018-10-04 12:13:48 +0200

    Fix lint errors from select input

--- src/components/SelectInput/SelectInput.vue
+++ src/components/SelectInput/SelectInput.vue
@@ -2,49 +2,68 @@
 
 <template>
   <div class="select-input">
-    <label :for="id" class="select-input__label" v-text="label"></label>
-    <select :id="id" class="select-input__field" :class="errorClass" :disabled="disabled">
-      <option v-for="option in options" :value="option">{{ option }}</option>
+    <label
+      class="select-input__label"
+      v-text="label"
+      :for="id"
+    />
+    <select
+      class="select-input__field"
+      :id="id"
+      :class="errorClass"
+      :disabled="disabled"
+    >
+      <option
+        v-for="option in options"
+        :value="option"
+      >
+        {{ option }}
+      </option>
     </select>
-    <div v-if="hasError" class="select-input__error-message ">{{ errorMessage }}</div>
+    <div
+      class="select-input__error-message"
+      v-if="hasError"
+    >
+      {{ errorMessage }}
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'TextInput',
-    props: {
-      id: {
-        type: String,
-        required: true
-      },
-      disabled: {
-        type: Boolean,
-        default: false
-      },
-      errorMessage: {
-        type: String,
-        default: ''
-      },
-      hasError: {
-        type: Boolean,
-        default: false
-      },
-      label: {
-        type: String,
-        required: true
-      },
-      options: {
-        type: Array,
-        default: () => []
-      }
+export default {
+  name: 'TextInput',
+  props: {
+    id: {
+      type: String,
+      required: true
     },
-    computed: {
-      errorClass () {
-        if (this.hasError) return 'select-input__field--error'
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    errorMessage: {
+      type: String,
+      default: ''
+    },
+    hasError: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    errorClass () {
+      if (this.hasError) return 'select-input__field--error'
 
-        return ''
-      }
+      return ''
     }
   }
+}
 </script>
