cf6d514817495355ecedbd7b24a5308b8a42bdc7
Anna Collins <annacollins85@gmail.com>
2018-10-05 12:40:55 +0200

    add stories folder and file for Form

---
+++ src/components/Form/_stories/Form.stories.js
@@ -0,0 +1,32 @@
+import { storiesOf } from '@storybook/vue'
+import centered from 'gss'
+
+import TextInput from '../TextInput/TextInput'
+import SelectInput from '../SelectInput/SelectInput'
+
+storiesOf('Basic Components', module)
+  .addDecorator(centered)
+  .add('Text Input', () => ({
+    components: { TextInput },
+    data () {
+      return {
+        errorMessage: 'You have an error'
+      }
+    },
+    template: '<TextInput id="paco" label="jujuju" :hasError="true" :errorMessage="errorMessage"/>'
+  }))
+  .add('Select Input', () => ({
+    components: { SelectInput },
+    data () {
+      return {
+        options: ['Anna', 'Marina', 'Mavi', 'Xabi'],
+        errorMessage: 'You have an error'
+      }
+    },
+    template: `<SelectInput
+                 id="blabla"
+                 label="select field"
+                 :errorMessage="errorMessage"
+                 :hasError="true"
+                 :options="options" />`
+  }))
