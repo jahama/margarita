e9ffbc2710464bf8cadd6cbfc0a6a4e7c96b31bf
Anna Collins <annacollins85@gmail.com>
2018-10-09 11:35:15 +0200

    Export radio button and checkbox input components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import GridColumn from './components/Grid/GridColumn/GridColumn.vue'
 import GridContainer from './components/Grid/GridContainer/GridContainer.vue'
 import GridRow from './components/Grid/GridRow/GridRow.vue'
+<<<<<<< HEAD
 import ButtonInput from './components/Form/ButtonInput/ButtonInput.vue'
+=======
+import CheckboxInput from './components/Form/CheckboxInput/CheckboxInput.vue'
+import RadioButton from './components/Form/RadioButton/RadioButton.vue'
+>>>>>>> Export radio button and checkbox input components
 import SelectInput from './components/Form/SelectInput/SelectInput.vue'
 import TextInput from './components/Form/TextInput/TextInput.vue'
 
@@ -11,7 +16,9 @@ export {
   GridRow,
   ButtonInput,
   SelectInput,
-  TextInput
+  TextInput,
+  CheckboxInput,
+  RadioButton
 }
 
 export default {
@@ -19,7 +26,12 @@ export default {
     Vue.component('GridColumn', GridColumn)
     Vue.component('GridContainer', GridContainer)
     Vue.component('GridRow', GridRow)
+<<<<<<< HEAD
     Vue.component('ButtonInput', ButtonInput)
+=======
+    Vue.component('CheckboxInput', CheckboxInput)
+    Vue.component('RadioButton', RadioButton)
+>>>>>>> Export radio button and checkbox input components
     Vue.component('SelectInput', SelectInput)
     Vue.component('TextInput', TextInput)
   }
