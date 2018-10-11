545b5ab9edf1cc592625c630eda2a16030cdf021
Anna Collins <annacollins85@gmail.com>
2018-10-11 11:18:10 +0200

    resolve conflicts merging button component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import GridColumn from './components/Grid/GridColumn/GridColumn.vue'
 import GridContainer from './components/Grid/GridContainer/GridContainer.vue'
 import GridRow from './components/Grid/GridRow/GridRow.vue'
-<<<<<<< HEAD
 import ButtonInput from './components/Form/ButtonInput/ButtonInput.vue'
-=======
 import CheckboxInput from './components/Form/CheckboxInput/CheckboxInput.vue'
 import RadioButton from './components/Form/RadioButton/RadioButton.vue'
->>>>>>> Export radio button and checkbox input components
 import SelectInput from './components/Form/SelectInput/SelectInput.vue'
 import TextInput from './components/Form/TextInput/TextInput.vue'
 
@@ -26,12 +23,9 @@ export default {
     Vue.component('GridColumn', GridColumn)
     Vue.component('GridContainer', GridContainer)
     Vue.component('GridRow', GridRow)
-<<<<<<< HEAD
     Vue.component('ButtonInput', ButtonInput)
-=======
     Vue.component('CheckboxInput', CheckboxInput)
     Vue.component('RadioButton', RadioButton)
->>>>>>> Export radio button and checkbox input components
     Vue.component('SelectInput', SelectInput)
     Vue.component('TextInput', TextInput)
   }
