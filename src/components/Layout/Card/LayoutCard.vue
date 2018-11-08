fee3e709c97d3ac0d6e0581e03fa2db444dc7acd
Marina Aisa <marina@marinaaisa.com>
2018-11-08 18:14:46 +0100

    Add layout card component

---
+++ src/components/Layout/Card/LayoutCard.vue
@@ -0,0 +1,44 @@
+<style lang="scss" src="./LayoutCard.scss"></style>
+
+<template>
+  <div
+    :class="getClasses"
+    class="layout-card"
+  >
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+
+  props: {
+    gray: {
+      type: Boolean,
+      default: false
+    },
+
+    hasMarginTop: {
+      type: Boolean,
+      default: false
+    },
+
+    white: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    getClasses () {
+      const classes = []
+
+      if (this.hasMarginTop) classes.push('layout-card--has-margin-top')
+      if (this.gray) classes.push('layout-card--gray')
+      if (this.white) classes.push('layout-card--white')
+
+      return classes
+    }
+  }
+}
+</script>
