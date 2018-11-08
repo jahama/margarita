63b4163f567b8db129ae3b10e6fef8260e70ab45
Carlos Moreno Villodre <carlos.moreno.villodre@gmail.com>
2018-11-08 14:47:52 +0100

    Use correct styles for buttons inside header

--- src/components/TheHeader/TheHeader.vue
+++ src/components/TheHeader/TheHeader.vue
@@ -1,24 +1,36 @@
-<style lang="scss" src="./TheHeader.scss"></style>
+<style lang="scss" src="./TheHeader.scss" scoped></style>
 
 <template>
   <header class="header">
-    <div class="header__logo">
-      <the-burger @click="onClickBurger" />
-      <a
-        href="https://www.holaluz.com"
-        class="header-logo__image"
-      ></a>
-    </div>
-    <button-input
-      tag="a"
-      href="https://alta.holaluz.com/es/"
-      text="Contratar"
-      type="gradient"
-    />
+    <grid-container fluid>
+      <grid-row>
+        <grid-column class="grid-col--6">
+          <div class="header__logo">
+            <the-burger @click="onClickBurger" />
+            <a
+              href="https://www.holaluz.com"
+              class="header-logo__image"
+            ></a>
+          </div>
+        </grid-column>
+        <grid-column class="grid-col--6">
+          <button-input
+            tag="a"
+            href="https://alta.holaluz.com/es/"
+            text="Contratar"
+            type="gradient"
+          />
+        </grid-column>
+      </grid-row>
+    </grid-container>
   </header>
 </template>
 
 <script>
+import GridContainer from '../Grid/GridContainer/GridContainer'
+import GridRow from '../Grid/GridRow/GridRow'
+import GridColumn from '../Grid/GridColumn/GridColumn'
+
 import TheBurger from './components/TheBurger/TheBurger'
 import ButtonInput from '../Form/ButtonInput/ButtonInput'
 
@@ -33,6 +45,9 @@ export default {
   },
 
   components: {
+    GridContainer,
+    GridRow,
+    GridColumn,
     TheBurger,
     ButtonInput
   },
