c2c41bde00dce10f1694533eb4f4b629c9d0842d
Biel <bielbcm@gmail.com>
2018-11-21 18:07:03 +0100

    [ITS-880] Update eslint with lint requirements

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,21 +14,22 @@ module.exports = {
     'vue'
   ],
   rules: {
+    'array-bracket-spacing': [ 'error', 'always' ],
     'babel/semi': 0,
     'generator-star-spacing': 'off',
-    'indent': [2, 2, {'SwitchCase': 1}],
-    'lines-between-class-members': ['error', 'always'],
+    'indent': [ 2, 2, {'SwitchCase': 1} ],
+    'lines-between-class-members': [ 'error', 'always' ],
     'space-before-blocks': 'error',
-    'lines-around-directive': ['error', 'always'],
+    'lines-around-directive': [ 'error', 'always' ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'vue/order-in-components': 0,
-    'vue/max-attributes-per-line': [2, {
+    'vue/max-attributes-per-line': [ 2, {
       'singleline': 2,
       'multiline': {
         'max': 2,
         'allowFirstLine': false
       }
-    }]
+    } ]
   }
 }
