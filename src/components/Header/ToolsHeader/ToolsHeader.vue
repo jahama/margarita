ca46eccdf0c7bd5f8113a71578734b486a700bd5
Anna Collins <annacollins85@gmail.com>
2018-11-21 17:23:47 +0100

    Complete tools header and CSS

--- src/components/Header/ToolsHeader/ToolsHeader.vue
+++ src/components/Header/ToolsHeader/ToolsHeader.vue
@@ -3,23 +3,77 @@
 <template>
   <div class="tools-header">
     <header class="tools-header__toolbar">
-      <TheBurger/>
-      <div>a</div>
-      <div>b</div>
+      <div class="tools-header__section">
+        <TheBurger
+          :is-close="burgerState"
+          tools-header
+          @click="toggleBurgerState"
+        />
+      </div>
+      <div class="tools-header__section">
+        <h3
+          class="tools-header__page-title"
+          v-text="pageTitle"
+        />
+      </div>
+      <div class="tools-header__section">
+        <UserActions
+          :logged-user="loggedUser"
+          :user-role="userRole"
+          @logout="emitLogoutEvent"
+        />
+      </div>
     </header>
-    <nav class="tools-header__navbar">
-    </nav>
+    <nav
+      v-if="burgerState"
+      class="tools-header__navbar"
+    />
   </div>
 </template>
 
 <script>
 import TheBurger from '../common/TheBurger/TheBurger'
+import UserActions from './components/UserActions/UserActions'
 
 export default {
   name: 'ToolsHeader',
 
   components: {
-    TheBurger
+    TheBurger,
+    UserActions
   },
+
+  props: {
+    pageTitle: {
+      default: '',
+      type: String
+    },
+
+    loggedUser: {
+      default: '',
+      type: String
+    },
+
+    userRole: {
+      default: '',
+      type: String
+    }
+  },
+
+  data () {
+    return {
+      burgerState: false
+    }
+  },
+
+  methods: {
+    toggleBurgerState () {
+      this.burgerState = !this.burgerState
+    },
+
+    emitLogoutEvent () {
+      this.$emit('logout')
+    }
+  }
 }
 </script>
