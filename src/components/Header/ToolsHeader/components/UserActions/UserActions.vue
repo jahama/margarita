17c5a7fe1754770ba2d9c1b1c018cc8c515203e1
Anna Collins <annacollins85@gmail.com>
2018-11-21 17:24:42 +0100

    Add UserActions component for internal header

---
+++ src/components/Header/ToolsHeader/components/UserActions/UserActions.vue
@@ -0,0 +1,42 @@
+<style lang="scss" src="./UserActions.scss"></style>
+
+<template>
+  <div class="user-actions">
+    <div
+      class="user-actions__logged-user"
+      v-text="`${loggedUser}  -  `"
+    />
+    <div
+      class="user-actions__user-role"
+      v-text="userRole"
+    />
+    <div
+      class="user-actions__icon"
+      @click="emitClickEvent"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserActions',
+
+  props: {
+    loggedUser: {
+      type: String,
+      default: ''
+    },
+
+    userRole: {
+      type: String,
+      default: ''
+    }
+  },
+
+  methods: {
+    emitClickEvent () {
+      this.$emit('logout')
+    }
+  }
+}
+</script>
