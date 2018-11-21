1f1633ec45af0199393dc56c321ffe3b461bef5e
Carlos Moreno Villodre <carlos.moreno.villodre@gmail.com>
2018-11-21 09:25:13 +0100

    Add WebNavbar dropdown

--- src/components/Header/WebHeader/components/WebNavbar/WebNavbar.vue
+++ src/components/Header/WebHeader/components/WebNavbar/WebNavbar.vue
@@ -1,7 +1,33 @@
+<style lang="scss" src="./WebNavbar.scss"></style>
+
 <template>
   <ul class="navbar">
-    <li class="navbar__link" v-for="link in links" :key="link.label">
-      <a :href="link.link" v-text="link.label"></a>
+    <li
+      class="navbar__link"
+      v-for="link in links"
+      :key="link.label"
+    >
+      <a
+        class="navbar__anchor"
+        :href="link.link"
+        v-text="link.label
+      "/>
+      <ul
+        class="navbar__dropdown"
+        v-if="link.items"
+      >
+        <li
+          class="dropdown__item"
+          v-for="item in link.items"
+          :key="item.label"
+        >
+          <a
+            class="dropdown__anchor"
+            :href="item.link"
+            v-text="item.label"
+          />
+        </li>
+      </ul>
     </li>
   </ul>
 </template>
@@ -16,59 +42,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-@import "../../../../../scss/margarita";
-
-.navbar {
-  display: none;
-
-  @include mq($from: lg) {
-    display: flex;
-    justify-content: space-between;
-    margin: 0;
-    padding: 0;
-  }
-
-  &__link {
-    display: flex;
-    list-style: none;
-    height: 2rem;
-    line-height: 1.3;
-
-    a {
-      text-decoration: none;
-      color: get-color("gray");
-      height: 100%;
-
-      &:hover {
-        color: get-color("dark-hot-pink");
-      }
-
-      &.active {
-        @include font-weight(medium);
-
-        position: relative;
-        color: get-color("tundora");
-
-        &:after {
-          background-image: linear-gradient(
-            to right,
-            get-color("crimson") 0%,
-            get-color("school-bus-yellow") 100%
-          );
-          background-repeat: no-repeat;
-          display: block;
-          position: absolute;
-          bottom: 0;
-          z-index: 11;
-          width: 100%;
-          height: .125rem;
-          content: "";
-        }
-      }
-    }
-  }
-
-}
-</style>
