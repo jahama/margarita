d3d0bc150f08776942b39dc62c5e4707eab141a7
Biel <bielbcm@gmail.com>
2018-12-18 16:46:56 +0100

    [ITS-973] Add new component DataGrid

---
+++ src/components/Components/DataGrid/DataGrid.vue
@@ -0,0 +1,94 @@
+<style lang="scss" src="./DataGrid.scss" scoped></style>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th
+          v-for="column in columns"
+          :key="column.title"
+          :class="sortClass(column.value, column.sortable)"
+          @click="sort(column)"
+        >
+          <span class="column-title">{{ column.title }}</span>
+          <span
+            v-if="column.sortable"
+            class="sort-arrow"
+          />
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="contract in data"
+        :key="contract.id"
+      >
+        <td
+          v-for="rowCell in contract"
+          :key="rowCell.keyValue"
+        >
+          <span
+            :is="rowCell.component"
+            v-if="rowCell.component"
+            v-bind="rowCell.componentData"
+          />
+          <span
+            v-else
+            v-text="rowCell"
+          />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'DataGrid',
+
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+
+    columns: {
+      type: Array,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      currentSort: '',
+      currentSortDir: 'ASC'
+    }
+  },
+
+  methods: {
+    sort (column) {
+      if (!column.sortable) return
+      if (column === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === 'ASC' ? 'DESC' : 'ASC'
+      } else {
+        this.currentSort = column
+        this.currentSortDir = 'ASC'
+      }
+      this.$emit('sort', { column: this.currentSort, dir: this.currentSortDir })
+    },
+
+    sortClass (value, isSortable) {
+      let sortableClass = ''
+      if (isSortable) {
+        sortableClass = 'sort-cursor-pointer'
+      }
+
+      if (this.currentSort.value === value) {
+        return `sort-arrow--${this.currentSortDir.toLowerCase()} ${sortableClass}`
+      }
+
+      return `sort-arrow--unsorted ${sortableClass}`
+    }
+  }
+}
+</script>
