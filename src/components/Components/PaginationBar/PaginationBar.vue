ad887f742035330edb732efa7546896cdcae851f
Biel <bielbcm@gmail.com>
2018-12-18 17:19:46 +0100

    [ITS-973] Add PaginationBar component

---
+++ src/components/Components/PaginationBar/PaginationBar.vue
@@ -0,0 +1,79 @@
+<style lang="scss" src="./PaginationBar.scss" scoped></style>
+
+<template>
+  <div class="pagination-bar">
+    <div
+      class="pagination-bar__left"
+      :class="{ 'pagination-bar--invisible': isStart }"
+    >
+      <div
+        class="pagination-bar__start"
+        @click="pagination('start')"
+      />
+      <div
+        class="pagination-bar__back"
+        @click="pagination('back')"
+      />
+    </div>
+    <div class="pagination-bar__current">
+      <slot />
+    </div>
+    <div
+      class="pagination-bar__right"
+      :class="{ 'pagination-bar--invisible': isEnd }"
+    >
+      <div
+        class="pagination-bar__forward"
+        @click="pagination('forward')"
+      />
+      <div
+        class="pagination-bar__end"
+        @click="pagination('end')"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaginationBar',
+
+  props: {
+    totalItems: {
+      type: Number,
+      default: 0
+    },
+
+    isEnd: {
+      type: Boolean,
+      default: false
+    },
+
+    isStart: {
+      type: Boolean,
+      default: false
+    },
+
+    from: {
+      type: Number,
+      default: 0
+    },
+
+    paginationContent: {
+      type: Object,
+      default: () => {}
+    },
+
+    to: {
+      type: Number,
+      default: 0
+    }
+  },
+
+  methods: {
+    pagination (direction) {
+      this.$emit('pagination', direction)
+    }
+  }
+}
+</script>
