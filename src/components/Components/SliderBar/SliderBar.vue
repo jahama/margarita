c1e242af9c37bc56b76766f42792d0143e22d050
Biel <bielbcm@gmail.com>
2019-01-08 16:16:29 +0100

    [ITS-580] SliderBar initial commit

---
+++ src/components/Components/SliderBar/SliderBar.vue
@@ -0,0 +1,24 @@
+<style lang="scss" src="./SliderBar.scss" scoped></style>
+
+<template>
+  <VueSlider ref="slider" v-model="value" />
+</template>
+
+<script>
+import VueSlider from 'vue-slider-component'
+
+export default {
+  name: 'SliderBar',
+
+  components: {
+    VueSlider
+  },
+
+  data () {
+    return {
+      value: 10
+    }
+  }
+
+}
+</script>
---
+++ src/components/Components/_stories/SliderBar.stories.js
@@ -0,0 +1,31 @@
+import { storiesOf } from '@storybook/vue'
+import { withKnobs, number } from '@storybook/addon-knobs/vue'
+import { withMarkdownNotes } from '@storybook/addon-notes'
+
+import SliderBarNotes from './notes/SliderBar.md'
+import GridColumn from '../../Grid/GridColumn/GridColumn'
+import SliderBar from '../SliderBar/SliderBar'
+
+storiesOf('Components', module)
+  .addDecorator(withKnobs)
+
+  .add('Slider Bar', withMarkdownNotes(SliderBarNotes)(() => {
+    const value = number('Pill Text', 1)
+
+    return ({
+      components: { SliderBar, GridColumn },
+
+      template: `
+        <GridColumn>
+          <SliderBar
+            :value="value"
+          />
+        </GridColumn>`,
+
+      data () {
+        return {
+          value
+        }
+      }
+    })
+  }))
