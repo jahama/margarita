7fff7508f15b5a5ed2fbe56663c719974c27776f
Carlos Moreno Villodre <carlos.moreno.villodre@gmail.com>
2019-01-17 14:40:49 +0100

    [WEB-1435] Fix required props in SelectInput tests

--- test/unit/components/Form/SelectInput.spec.js
+++ test/unit/components/Form/SelectInput.spec.js
@@ -3,7 +3,12 @@ import SelectInput from '@/components/Form/SelectInput/SelectInput.vue'
 
 describe('SelectInput', () => {
   it('should have a select with a class', () => {
-    const wrapper = shallowMount(SelectInput)
+    const wrapper = shallowMount(SelectInput, {
+      props: {
+        options: [],
+        label: 'Label'
+      }
+    })
 
     expect(wrapper.contains('select')).toBe(true)
     expect(wrapper.contains('.select-input')).toBe(true)
