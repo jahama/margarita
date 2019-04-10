e4c8f577cd80b88464f572774e6803de8660a159
Adrià Fontcuberta <afontcu@gmail.com>
2019-04-10 08:22:04 +0200

    Update linting rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,30 +22,20 @@ module.exports = {
     'babel/semi': 0,
     'comma-dangle': [ 'error', 'always-multiline' ],
     'generator-star-spacing': 'off',
-    'indent': [ 2, 2, { 'SwitchCase': 1 } ],
+    'indent': [ 'error', 2, { 'SwitchCase': 1 } ],
     'lines-between-class-members': [ 'error', 'always' ],
     'space-before-blocks': 'error',
     'lines-around-directive': [ 'error', 'always' ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'vue/order-in-components': 0,
-    'vue/max-attributes-per-line': [ 2, {
+    'vue/max-attributes-per-line': [ 'error', {
       'singleline': 2,
       'multiline': {
         'max': 2,
         'allowFirstLine': false,
       },
     } ],
-    'vue/component-name-in-template-casing': [
-      'error',
-      'PascalCase',
-      {
-        'ignores': [
-          'nuxt',
-          'nuxt-child',
-          'nuxt-link',
-        ],
-      },
-    ],
+    'vue/component-name-in-template-casing': ['error', 'kebab-case' ],
   },
 }
