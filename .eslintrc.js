89dfae4992da4b53307a78a757db136c0df4c9df
Adrià Fontcuberta <afontcu@gmail.com>
2019-05-13 09:38:54 +0200

    Add Prettier+Stylelint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,32 +10,19 @@ module.exports = {
     es6: true,
   },
   extends: [
-    'standard',
     'plugin:vue/recommended',
+    'eslint:recommended',
+    // Vue specific formatting rules to prettier
+    'prettier/vue',
+    // Treat prettier errors as linting errors
+    // Disable certain rules that interfere with prettier
+    'plugin:prettier/recommended'
   ],
   plugins: [
     'vue',
   ],
   rules: {
-    'array-bracket-spacing': [ 'error', 'always' ],
-    'arrow-parens': [ 'error', 'as-needed' ],
-    'babel/semi': 0,
-    'comma-dangle': [ 'error', 'always-multiline' ],
-    'generator-star-spacing': 'off',
-    'indent': [ 'error', 2, { 'SwitchCase': 1 } ],
-    'lines-between-class-members': [ 'error', 'always' ],
-    'space-before-blocks': 'error',
-    'lines-around-directive': [ 'error', 'always' ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    'vue/order-in-components': 0,
-    'vue/max-attributes-per-line': [ 'error', {
-      'singleline': 2,
-      'multiline': {
-        'max': 2,
-        'allowFirstLine': false,
-      },
-    } ],
-    'vue/component-name-in-template-casing': ['error', 'kebab-case' ],
   },
 }
---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    // Enables the stylelint-plugin-prettier plugin.
+    // Enables the prettier/prettier rule.
+    // Extends the stylelint-config-prettier configuration.
+    'stylelint-prettier/recommended',
+  ],
+}
