cda7da8ca4d09dadf7a14105424224c2bb02a885
Carlos Moreno Villodre <carlos.moreno.villodre@gmail.com>
2019-05-29 11:56:43 +0200

    Add style-lint-concentric (seems that order defers from sass-lint)

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,6 +3,8 @@ module.exports = {
     // Enables the stylelint-plugin-prettier plugin.
     // Enables the prettier/prettier rule.
     // Extends the stylelint-config-prettier configuration.
+    // Extends the stylelint-config-concentric-order configuration.
     'stylelint-prettier/recommended',
+    'stylelint-config-concentric-order',
   ],
 }
