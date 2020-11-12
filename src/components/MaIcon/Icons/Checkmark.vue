6b8372816b9f904ce8ae1b3a5fce1b847812bca3
pauortegathoughtworks <48923876+pauortegathoughtworks@users.noreply.github.com>
2020-11-12 14:32:18 +0100

    [KAN-10271] Add reassignment SVGs (#282)
    * [KAN-10271] Add reassignment SVGs
    
    co-authored by: Andrea Ristori <aristori@thoughtworks.com>
    
    * Andrea | Refactor of SVG code to fit viewbox
    
    * Andrea | Added Mail Contact Icon
    
    Co-authored-by: ristoman <aristori@thoughtworks.com>

---
+++ src/components/MaIcon/Icons/Checkmark.vue
@@ -0,0 +1,7 @@
+<template>
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M0 6.90643L6.71533 14L18 2.31881L15.7993 0L6.71533 9.26552L2.31947 4.62073L0 6.90643Z"
+  />
+</template>
