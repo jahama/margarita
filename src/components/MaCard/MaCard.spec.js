fbaf8bcd8f29a1b325ff9d072310764c544f84ac
Àlex Castelo <37211588+alexcastelo95@users.noreply.github.com>
2021-05-27 14:55:15 +0200

    docs: add-ma-card-docs (#423)
    * Add MaCard Figma emded docs
    
    * Add simple tests for MaCard component
    
    * Commit suggested changes
    
    Co-authored-by: Alejandro <alejandrocastelo@MacBook-Pro-de-Alejandro.local>

---
+++ src/components/MaCard/MaCard.spec.js
@@ -0,0 +1,35 @@
+import { render } from '@margarita/margarita-test-utils'
+import MaCard from './MaCard'
+
+describe('Card', () => {
+  test('renders a card element by default', () => {
+    const { contentWrapper } = renderComponent()
+    expect(contentWrapper).toBeVisible()
+  })
+
+  test('renders the default grey variant when we do not specify the prop color', () => {
+    const { contentWrapper } = renderComponent()
+
+    expect(contentWrapper).toHaveClass('ma-card--gray')
+  })
+
+  test('renders the white variant when we specify the white prop color', () => {
+    const { contentWrapper } = renderComponent({
+      color: 'white',
+    })
+
+    expect(contentWrapper).toHaveClass('ma-card--white')
+  })
+})
+
+function renderComponent(props = {}) {
+  const utils = render(MaCard, {
+    props,
+    slots: { default: 'content' },
+  })
+  const contentWrapper = utils.getByText('content')
+  return {
+    ...utils,
+    contentWrapper,
+  }
+}
