afb5625c8833a58b52f7f7670642661693b02149
Adrià Fontcuberta <afontcu@gmail.com>
2021-07-08 17:34:14 +0200

    feat: Update shadow tokens to match spec (#430)

--- src/tokens/shadow.js
+++ src/tokens/shadow.js
@@ -1,5 +1,7 @@
 module.exports = {
-  light: 'rgba(0, 0, 0, 0.027451)',
-  medium: 'rgba(0, 0, 0, 0.0980392)',
-  dark: 'rgba(0, 0, 0, 0.498)',
+  xsmall: '0 0 6px 0 rgba(33, 33, 33, 0.05)',
+  small: '0 3px 17px 0 rgba(33, 33, 33, 0.1)',
+  medium: '0 3px 17px 0 rgba(33, 33, 33, 0.25)',
+  large: '0 8px 19px 0 rgba(33, 33, 33, 0.25)',
+  fancy: '0 9px 18px 0 rgba(200, 192, 171, 1)',
 }
