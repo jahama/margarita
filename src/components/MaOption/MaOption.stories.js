3e50e965b61ba4fae120ea0429e14802bcd3b772
Blai Samitier <blai.samitier@gmail.com>
2021-08-23 11:50:03 +0200

    docs: add ma option states storybook (#447)

--- src/components/MaOption/MaOption.stories.js
+++ src/components/MaOption/MaOption.stories.js
@@ -1,15 +1,13 @@
 import MaOption from './MaOption'
 import docsRadio from '../../../docs/components/form-elements/MaOption/Radio.docs.mdx'
 import docsCheckbox from '../../../docs/components/form-elements/MaOption/Checkbox.docs.mdx'
+import docsCard from '../../../docs/components/MaCard.docs.mdx'
 import { tones } from '@margarita/tokens'
 
 export default {
   title: 'Components/Form Elements/Option',
   component: MaOption,
   argTypes: {
-    type: {
-      control: false,
-    },
     'v-model': {
       control: false,
     },
@@ -29,57 +27,65 @@ export default {
   },
 }
 
-const RadioTemplate = (args, { argTypes }) => {
-  return {
-    data: () => ({
-      selectedOption: 'radio1',
-    }),
-    props: Object.keys(argTypes),
-    template: `
-      <ma-stack space="small">
-        <ma-option v-model="selectedOption" v-bind="$props" value="radio1" id="radio1" @change="change">
-          My first radio
-        </ma-option>
-        <ma-option v-model="selectedOption" v-bind="$props" value="radio2" id="radio2" @change="change">
-          My second radio
-        </ma-option>
-      </ma-stack>
-    `,
-  }
-}
+export const Option = (_, { argTypes }) => ({
+  data: () => ({
+    selectedOption: 'radio1',
+  }),
+  props: Object.keys(argTypes),
+  template: `
+    <ma-stack space="small">
+      <ma-option v-model="selectedOption" v-bind="$props" value="radio1" id="radio1" @change="change">
+        My first radio
+      </ma-option>
+      <ma-option v-model="selectedOption" v-bind="$props" value="radio2" id="radio2" @change="change">
+        My second radio
+      </ma-option>
+    </ma-stack>
+  `,
+})
 
-export const Radio = RadioTemplate.bind({})
-Radio.args = {
-  type: 'radio',
-}
-Radio.parameters = {
+const OptionStates = ({ type = 'radio', card = false }) => () => ({
+  data: () => ({
+    selectedOption: 'radio1',
+  }),
+  template: `
+    <ma-stack space="small">
+      <ma-option type="${type}" :card="${card}">
+        Default
+      </ma-option>
+      <ma-option v-model="selectedOption" value="radio1" type="${type}" :card="${card}">
+      Active
+      </ma-option>
+      <ma-option type="${type}" :card="${card}" disabled >
+        Disabled
+      </ma-option>
+      <ma-option type="${type}" :card="${card}" has-error >
+        Error
+      </ma-option>
+      <ma-option type="${type}" :card="${card}" ref="focusElem" >
+        Focus
+      </ma-option>
+    </ma-stack>
+  `,
+  mounted() {
+    this.$refs.focusElem.$el.focus()
+  },
+})
+
+export const RadioStates = OptionStates({ type: 'radio' })
+RadioStates.bind({})
+RadioStates.parameters = {
   docs: { page: docsRadio },
 }
 
-const CheckboxTemplate = (args, { argTypes }) => {
-  return {
-    data: () => ({
-      checkedOption: [],
-    }),
-    props: Object.keys(argTypes),
-    template: `
-      <ma-stack space="small">
-        <ma-option v-model="checkedOption" v-bind="$props" value="checkbox1" id="checkbox1" @change="change">
-          My first checkbox
-        </ma-option>
-        <ma-option v-model="checkedOption" v-bind="$props" value="checkbox2" id="checkbox2" @change="change">
-          My second checkbox
-        </ma-option>
-      </ma-stack>
-    `,
-  }
-}
-
-export const Checkbox = CheckboxTemplate.bind({})
-Checkbox.args = {
-  type: 'checkbox',
-}
-
-Checkbox.parameters = {
+export const CheckboxStates = OptionStates({ type: 'checkbox' })
+CheckboxStates.bind({})
+CheckboxStates.parameters = {
   docs: { page: docsCheckbox },
+}
+
+export const CardStates = OptionStates({ type: 'checkbox', card: true })
+CardStates.bind({})
+CardStates.parameters = {
+  docs: { page: docsCard },
 }
