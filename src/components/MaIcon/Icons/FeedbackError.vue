5b6de5bcfc337e39bea8341a790f54ba6fc77e4c
IbanCurdu <icurdu@gmail.com>
2021-08-27 20:50:01 +0200

    Added some new icons (1/2)
    Added new feedback, communication, person and document icons.

---
+++ src/components/MaIcon/Icons/FeedbackError.vue
@@ -0,0 +1,8 @@
+<template>
+  <path
+    clip-rule="evenodd"
+    fill-rule="evenodd"
+    fill="#cb1010"
+    d="m12 2c-5.52 0-10 4.48-10 10s4.48 10 10 10 10-4.48 10-10-4.48-10-10-10zm0 11c-.55 0-1-.45-1-1v-4c0-.55.45-1 1-1s1 .45 1 1v4c0 .55-.45 1-1 1zm-1 2v2h2v-2z"
+  />
+</template>
