87d4e57b272d4f2284fb032bab0140050380e5bd
IbanCurdu <icurdu@gmail.com>
2021-11-01 18:11:47 +0100

    Added some new icons (2/2)
    Added the second batch of the icons to the folder.

---
+++ src/components/MaIcon/Icons/CreditCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <path
+    clip-rule="evenodd"
+    d="m20 4h-16c-1.11 0-1.99.89-1.99 2l-.01 12c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2v-12c0-1.11-.89-2-2-2zm-1 14h-14c-.55 0-1-.45-1-1v-5h16v5c0 .55-.45 1-1 1zm-15-10h16v-2h-16z"
+    fill-rule="evenodd"
+  />
+</template>
